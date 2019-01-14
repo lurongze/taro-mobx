8a8d2c7bc19366bb72bdbc954626fc9c191323b6
lurongze <1946755280@qq.com>
2019-01-14 18:25:43 +0800

    1

--- src/hoc/withLogin.js
+++ src/hoc/withLogin.js
@@ -1,23 +1,17 @@
 
 function withLogin() {
-
   return function demoComponent(Component) {
-
     class WithLogin extends Component {
-
       constructor(props) {
         super(props)
       }
-
       async componentWillMount() {
-        console.log('WithLogin-componentWillMount', this.props)
-
+        // console.log('WithLogin-componentWillMount', this.props)
         this.props.commonStore.login()
         // fixme 这里会触发两次的，暂时去掉吧
         // super.componentDidMount()
       }
     }
-
     return WithLogin
   }
 }
